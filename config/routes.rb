556e3bf8ade5de2d27f9b39b8cead53ec2be755d
takattty <r9r_celtics_t9t@icloud.com>
2020-09-13 22:54:00 +0900

    created routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace 'api' do
+    namespace 'v1' do
+      resources :calendar, only: [:index]
+      resources :reportindex, only: [:index]
+
+      scope '/auth' do
+        post '/login', to:'user_token#create'
+        get '/user', to:'users#show'
+        post '/user', to:'users#create'
+      end
+
+      resources :lesson, only: [:show, :create] do # lesson/:lessonid
+        resources :report, only: [:index, :update] # lesson/:lessonid/report OK!!!!!
+        resources :attendance, only: [:index, :create, :update] # lesson/:lessonid/attendance OK!!!!
+        resources :text, only: [:show] # lesson/:lessonid/text/:textid OK!!!
+        resources :task, only: [:create, :show] # lesson/:lessonid/task/:taskid  showはOK createは微妙。
+      end
+
+    end
+  end
 end
