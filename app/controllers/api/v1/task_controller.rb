81bc05eee7c309553eb3def950c377a5784e8df2
takattty <r9r_celtics_t9t@icloud.com>
2020-09-24 03:22:13 +0900

    fixed task controller

--- app/controllers/api/v1/task_controller.rb
+++ app/controllers/api/v1/task_controller.rb
@@ -3,14 +3,19 @@ class Api::V1::TaskController < ApplicationController
   before_action :set_task, only: [:create]
 
   def show
-    @task = Report.where("report_id = ? AND lesson_id = ?", @report_id, @lesson_id)
-    render json: { status: "SUCCESS", task_data: { lesson_id: @lesson_id, task_content: @task } }
+    # @task = Report.where("report_id = ? AND lesson_id = ?", @report_id, @lesson_id)
+    # render json: { status: "SUCCESS", task_data: { lesson_id: @lesson_id, task_content: @task } }
+    text = Text.find(@report_id)
+    text_name = text.text_name
+    task = Report.where("report_name = ?", text_name)
+    render json: { status: 200, task_data: { lesson_id: @lesson_id, task_content: task } }
+    
   end
 
   def create
     task = Task.new(set_task)
     if task.save
-      render json: { status: "SUCCESS", data: task }
+      render json: { status: 200, data: task }
     else
       render json: { status: "ERROR", data: task.errors }
     end
