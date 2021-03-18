51375ef61f05959e6c2c5b2b06e0ea2fd15ec378
takattty <r9r_celtics_t9t@icloud.com>
2021-03-18 23:57:09 +0900

    modify report index controller

--- app/controllers/api/v1/reportindex_controller.rb
+++ app/controllers/api/v1/reportindex_controller.rb
@@ -1,19 +1,15 @@
 class Api::V1::ReportindexController < ApplicationController
-  before_action :get_userid, :get_userid
+  before_action :get_userid
 
   def show
-    lessons = Account.find(@user_id)
-    lessons_lists = lessons.lesson_lists
-    lessons_array = lessons_lists.scan(/\d+/)
+    account = Account.find(@user_id)
+    lessons_lists = account.lessons
     report_hash = {}
-
-    lessons_array.each do |each_lesson_id|
-      lesson_id = each_lesson_id.to_i
-      @report = report_hash.merge!( each_lesson_id => Report.where("lesson_id = ?", lesson_id))
+    lessons_lists.each do |lesson|
+      @report = report_hash.merge!( lesson.id => lesson.reports )
     end
     render json: { status: "SUCCESS", calendar: { user_id: @user_id, report: @report } }
   end
-
 
   private
 
