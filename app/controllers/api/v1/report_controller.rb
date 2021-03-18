206552e9da29b9c6f9f155b2e981a9bbf21210de
takattty <r9r_celtics_t9t@icloud.com>
2021-03-18 23:36:59 +0900

    modify report controller

--- app/controllers/api/v1/report_controller.rb
+++ app/controllers/api/v1/report_controller.rb
@@ -3,17 +3,17 @@ class Api::V1::ReportController < ApplicationController
   before_action :set_report, only: [:update]
 
   def index
-    @report = Task.where("lesson_id = ?", @lesson_id)
-    render json: { status: "SUCCESS", report_data: { lesson_id: @lesson_id, text_content: @report } }
+    report = Task.where("lesson_id = ?", @lesson_id)
+    render json: { status: "SUCCESS", report_data: { lesson_id: @lesson_id, content: report } }
   end
 
   def update
-    @report_id = params[:id]
-    @report = Task.find(@report_id)
-    if @report.update(set_report)
-      render json: { status: "SUCCESS", message: "Updated the post", data: @report }
+    report_id = params[:id]
+    report = Task.find(report_id)
+    if report.update(set_report)
+      render json: { status: "SUCCESS", message: "Updated the post", data: report }
     else
-      render json: { status: "ERROR", message: "Not updated", data: @report.errors }
+      render json: { status: "ERROR", message: "Not updated", data: report.errors }
     end
 
   end
@@ -25,7 +25,7 @@ class Api::V1::ReportController < ApplicationController
   end
   
   def set_report
-    params.require(:report_update).permit(:lesson_id, :report_id, :user_id, :submitted_file, :submitted_date, :correction_file, :comment, :score)
+    params.require(:report_update).permit(:lesson_id, :report_id, :account_id, :submitted_date, :comment, :score)
   end
 
 end
