7dcc55fc0bc12ba119f70b4932809ad55412c207
takattty <r9r_celtics_t9t@icloud.com>
2021-03-19 00:07:56 +0900

    modify task controller

--- app/controllers/api/v1/task_controller.rb
+++ app/controllers/api/v1/task_controller.rb
@@ -3,19 +3,15 @@ class Api::V1::TaskController < ApplicationController
   before_action :set_task, only: [:create]
 
   def show
-    # @task = Report.where("report_id = ? AND lesson_id = ?", @report_id, @lesson_id)
-    # render json: { status: "SUCCESS", task_data: { lesson_id: @lesson_id, task_content: @task } }
-    text = Text.find(@report_id)
-    text_name = text.text_name
-    task = Report.where("report_name = ?", text_name)
-    render json: { status: 200, task_data: { lesson_id: @lesson_id, task_content: task } }
+    task = Report.find(@report_id)
+    render json: { status: "SUCCESS", task_data: { lesson_id: @lesson_id, content: task } }
     
   end
 
   def create
     task = Task.new(set_task)
     if task.save
-      render json: { status: 200, data: task }
+      render json: { status: "SUCCESS", data: task }
     else
       render json: { status: "ERROR", data: task.errors }
     end
@@ -30,7 +26,7 @@ class Api::V1::TaskController < ApplicationController
   end
 
   def set_task
-    params.require(:task_create).permit(:lesson_id, :report_id, :user_id, :submitted_file, :submitted_date, :correction_file, :comment, :score)
+    params.require(:task_create).permit(:lesson_id, :report_id, :user_id, :submitted_date, :comment, :score)
   end
 
 end
