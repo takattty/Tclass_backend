69594303987cea74af01d6e70aa8ff4973732a3f
takattty <r9r_celtics_t9t@icloud.com>
2021-03-19 00:55:35 +0900

    modify calendar controller

--- app/controllers/api/v1/calendar_controller.rb
+++ app/controllers/api/v1/calendar_controller.rb
@@ -1,49 +1,30 @@
 class Api::V1::CalendarController < ApplicationController
-  before_action :get_userid
+  before_action :get_user_id
 
   def show
-    lessons = Account.find(@user_id)
-    # p "lessons", lessons
-    lessons_lists = lessons.lesson_lists
-    lessons_array = lessons_lists.scan(/\d+/)
     lesson_hash = {}
     schedule_hash = {}
     report_hash = {}
 
-    lessons_array.each do |each_lesson_id|
-      lesson_id = each_lesson_id.to_i
-      @lesson = lesson_hash.merge!(each_lesson_id => Lesson.where("lesson_id = ?", lesson_id).to_a)
-      p "@lesson", @lesson
-      # p "@lesson.class", @lesson.class
-      # lessonssss = @lesson[lesson_id]
-      # p "lessonssss", lessonssss
-      # p "lessonssss.class", lessonssss.class
-      # p "lessonssss[0]", lessonssss[0]
-      # leson = lessonssss[0]
-      # p "leson.schedule_lists", leson.schedule_lists # 文字列の中にある、schedule_listsの配列。
-      # p "leson.class", leson.class
-      lesson_schedule_string = @lesson[each_lesson_id]
-      p "lesson_schedule_string", lesson_schedule_string
-      lesson_schedule = lesson_schedule_string
-      p "lesson_schedule", lesson_schedule
-      schedule_string = lesson_schedule[0]
-      p "schedule_string", schedule_string
-      schedule_strings = schedule_string.schedule_lists
-      lesson_schedule_array = schedule_strings.scan(/\d+/)
-      # p "lesson_schedule_array", esson_schedule_array
-      lesson_schedule_array.each do |each_schedule_id|
-        # p "each_schedule_id", each_schedule_id
-        schedule_id = each_schedule_id
-        @schedule = schedule_hash.merge!(each_schedule_id => LessonSchedule.where("schedule_id = ?", schedule_id))
+    account = Account.find(@user_id)
+    lessons = account.lessons
+    lessons.each do |lesson|
+      @lesson = lesson_hash.merge!(lesson.id => lesson)
+      schedules = lesson.schedules
+      schedules.each do |schedule|
+        @schedule = schedule_hash.merge!(schedule.id => schedule)
       end
-      @report = report_hash.merge!(each_lesson_id => Report.where("lesson_id = ?", lesson_id))
+      reports = lesson.reports
+      reports.each do |report|
+        @report = report_hash.merge!(report.id => report)
+      end
     end
     render json: { status: 200, calendar_content: { user_id: @user_id, lesson: @lesson, schedule: @schedule, report: @report } }
   end
 
   private
 
-  def get_userid
+  def get_user_id
     @user_id = params[:id]
   end
 end
