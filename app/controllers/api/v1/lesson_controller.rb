7fdf12f1bc5d97e49e298c8a0c3fa3b7cac52a65
takattty <r9r_celtics_t9t@icloud.com>
2021-03-18 23:31:41 +0900

    modify lesson controller

--- app/controllers/api/v1/lesson_controller.rb
+++ app/controllers/api/v1/lesson_controller.rb
@@ -3,15 +3,13 @@ class Api::V1::LessonController < ApplicationController
   before_action :set_lesson, only: [:create]
 
   def show
-    @text = Text.where("lesson_id = ?", @lesson_id)
-    render json: { status: "SUCCESS", text_data: { lesson_id: @lesson_id, text_content: @text} }
+    text = Text.where("lesson_id = ?", @lesson_id)
+    render json: { status: "SUCCESS", text_data: { lesson_id: @lesson_id, text_content: text } }
   end
 
   def create
-    p set_lesson.class
-    p set_lesson[:text_type]
-    text_type = set_lesson[:text_type]
-    if text_type == "資料"
+    file_type = set_lesson[:file_type]
+    if file_type == "資料"
       text = Text.new(set_lesson)
       if text.save
         render json: { status: "SUCCESS", data: text }
@@ -19,30 +17,19 @@ class Api::V1::LessonController < ApplicationController
         render json: { status: "ERROR", data: text.errors }
       end
 
-    elsif text_type == "レポート"
-      text_result_hash = {}
-      report_result_hash = {}
+    elsif file_type == "レポート"
       text = Text.new(set_lesson)
-      if text.save
-        @text_result = text_result_hash.merge!( status: "SUCCESS", data: text )
-      else
-        @text_result = text_result_hash.merge!( status: "ERROR", data: text.errors )
-      end
+      @text_result = text.save ? { status: "SUCCESS", data: text } : { status: "ERROR", data: text.errors }
 
-      created_report = 
+      created_report =
       { 
-        report_id: nil,
-        report_name: set_lesson[:text_name],
-        report_file: set_lesson[:text_file],
-        report_date: set_lesson[:text_date],
-        lesson_id: set_lesson[:lesson_id]
+        lesson_id: set_lesson[:lesson_id],
+        name: set_lesson[:name],
+        start_date: set_lesson[:start_date],
+        finish_date: set_lesson[:finish_date]
       }
       report = Report.new(created_report)
-      if report.save
-        @report_result = report_result_hash.merge!( status: "SUCCESS", data: report )
-      else
-        @report_result = report_result_hash.merge!( status: "ERROR", data: report.errors )
-      end
+      @report_result = report.save ? { status: "SUCCESS", data: report } : { status: "ERROR", data: report.errors }
       render json: { status: "SUCCESS", lesson_create: { text_result: @text_result, report_result: @report_result }}
     end
   end
@@ -54,7 +41,7 @@ class Api::V1::LessonController < ApplicationController
   end
 
   def set_lesson
-    params.require(:lesson_create).permit(:card_title, :text_name, :text_file, :text_type, :text_date, :text_times, :lesson_id)
+    params.require(:lesson_create).permit(:lesson_id, :card_title, :name, :file_type, :start_date, :finish_datem, :times)
   end
 
 end
